213b23f5ed844c18f1d44e8c23f6f9c7c94fd9da
N1k100n <92565385+N1k100n@users.noreply.github.com>
2023-02-21 20:34:59 +0300

    Create 1_6.py

---
+++ lesson8/tasks/1_6.py
@@ -0,0 +1,12 @@
+def swap_columns(a, i, j):
+    for k in range(len(a)):
+        a[k][i], a[k][j] = a[k][j], a[k][i]
+ 
+n, m = [int(i) for i in input().split()]
+a = [[int(j) for j in input().split()] for i in range(n)]
+i, j = [int(i) for i in input().split()]
+swap_columns(a, i, j)
+for g in a:
+    for k in g:
+        print(k,end=" ")
+    print()
